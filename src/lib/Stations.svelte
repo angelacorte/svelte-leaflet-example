3e1f1a53a766160b403be0b4a7cf160dd6462ca6
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-08-24 17:59:59 +0200

    feat: add module for charging stations

--- src/lib/Stations.svelte
+++ src/lib/Stations.svelte
@@ -1,12 +1,4 @@
 <script context="module">
-
-    import * as L from 'leaflet';
-    import 'leaflet/dist/leaflet.css';
-    import redIcon from '../assets/red-icon.png';
-    import orangeIcon from '../assets/orange-icon.png';
-    import blueIcon from '../assets/blue-icon.png';
-    import greenIcon from '../assets/green-icon.png';
-
     export const ChargingStationStatus = {
         FREE: "FREE",
         CHARGING: "CHARGING",
@@ -82,31 +74,5 @@
             status: ChargingStationStatus.RESERVED
         },
     ]
-
-    export let redMarker = L.icon({
-        iconUrl: redIcon,
-        iconSize:     [40, 40], // size of the icon
-        iconAnchor:   [5, 5], // point of the icon which will correspond to marker's location
-        popupAnchor:  [15, 5] // point from which the popup should open relative to the iconAnchor
-    });
-    export let blueMarker = L.icon({
-        iconUrl: blueIcon,
-        iconSize:     [40, 40], // size of the icon
-        iconAnchor:   [5, 5], // point of the icon which will correspond to marker's location
-        popupAnchor:  [15, 5] // point from which the popup should open relative to the iconAnchor
-    });
-
-    export let greenMarker = L.icon({
-        iconUrl: greenIcon,
-        iconSize:     [40, 40], // size of the icon
-        iconAnchor:   [5, 5], // point of the icon which will correspond to marker's location
-        popupAnchor:  [15, 5] // point from which the popup should open relative to the iconAnchor
-    });
-    export let orangeMarker = L.icon({
-        iconUrl: orangeIcon,
-        iconSize:     [40, 40], // size of the icon
-        iconAnchor:   [5, 5], // point of the icon which will correspond to marker's location
-        popupAnchor:  [15, 5] // point from which the popup should open relative to the iconAnchor
-    });
 </script>
 
